2d1f349ea96e9b030016d2c9cc4c7f0714ea6b63
Viktor Grigorov <84868186+ViktorGrigorov2910@users.noreply.github.com>
2021-06-29 23:59:03 +0300

    Update Demo.java
    Still in progress...

--- Demo.java
+++ Demo.java
@@ -4,21 +4,21 @@ import javax.swing.*;
 
 public abstract class Demo {
     public static void main(String[] args) {
-        String name = JOptionPane.showInputDialog("Enter your name");
+        String name = JOptionPane.showInputDialog("Enter your name:");
         JOptionPane.showMessageDialog(null, "What a cringe name, " + name);
 
-        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
+        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age:"));
         if (age < 18) {
-            JOptionPane.showMessageDialog(null, "Young BLOOD!");
+            JOptionPane.showMessageDialog(null, "Young G!");
         } else {
-            JOptionPane.showMessageDialog(null, "OLD SCHOOL VETERAN");
+            JOptionPane.showMessageDialog(null, "OK BOOMER!");
         }
 
-        double height = Double.parseDouble(JOptionPane.showInputDialog("Enter your height"));
+        double height = Double.parseDouble(JOptionPane.showInputDialog("Enter your height:"));
         if (height <= 1.50) {
-            JOptionPane.showMessageDialog(null, "HAHA You MIGET");
-        } else {
-            JOptionPane.showMessageDialog(null, "HAHA You tall ASS BI");
+            JOptionPane.showMessageDialog(null, "Kinda short.");
+        } else +{
+            JOptionPane.showMessageDialog(null, "You are " + height + " cm tall!");
         }
     }
 
